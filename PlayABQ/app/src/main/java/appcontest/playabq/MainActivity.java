5987f50fb8f456da146a26fc124903597c741c2a
ststromberg <ststromb@gmail.com>
2016-03-26 16:35:33 -0600

    Display Permission Warning
    Break the news softly that we are spying on the insufferable peasants.

--- PlayABQ/app/src/main/java/appcontest/playabq/MainActivity.java
+++ PlayABQ/app/src/main/java/appcontest/playabq/MainActivity.java
@@ -22,6 +22,7 @@ import android.view.MenuItem;
 import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -161,22 +162,13 @@ public class MainActivity extends AppCompatActivity
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        FINE_LOCATION_ACCESS_REQUEST);
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
+                Toast.makeText(this, "We are about to ask you for location permissions. " +
+                        "This allows us" + "to display parks and community centers preferenced by " +
+                        "closest location to you.", Toast.LENGTH_LONG).show();
             }
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    FINE_LOCATION_ACCESS_REQUEST);
         }
     }
 
